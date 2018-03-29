c94ec4304e27ae99e8b1102830d890c02640ae9e
Anthony Cros <cros.anthony@gmail.com>
2018-03-29 10:47:12 -0400

    Poor-man's (minimal) vcf extractor

---
+++ maf2vcf.sh
@@ -0,0 +1,23 @@
+#/bin/bash -e
+# 180328145621
+# usage: ./script.sh ./mafs > all.sorted.minimal.vcf
+
+input_dir=${1?} && shift
+
+function process() {
+    input_file=${1?} && shift
+  allele_index=${1?} && shift
+  
+  sample=$(basename ${input_file?} | cut -d'.' -f1)
+  
+  # 5=chr, 6=start, 11=ref, 12=tumor allele1, 13=tumor allele2
+  awk -F$'\t' '$11!=$'${allele_index?}'{print $5 "\t" $6 "\t\t" $11 "\t" $'${allele_index?}' "\t\t\tID=" "'${sample?}'" "@" NR "@" '${allele_index?}'}'
+}
+
+{
+  echo -e "##fileformat=VCFv4.1\n#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO"
+  for f in $(ls ${input_dir?}/*.maf.gz); do
+    zcat $f | tail -n+3 | process $f 12
+    zcat $f | tail -n+3 | process $f 13
+  done | sort -t$'\t' -k1,1 -k2n,2n
+} | gzip -c
