3097cd48f21d2ff2491b0fa777458e3a25b0621b
Anthony Cros <cros.anthony@gmail.com>
2018-03-29 10:50:51 -0400

    Slight improvements

--- maf2vcf.sh
+++ maf2vcf.sh
@@ -10,14 +10,14 @@ function process() {
   
   sample=$(basename ${input_file?} | cut -d'.' -f1)
   
-  # 5=chr, 6=start, 11=ref, 12=tumor allele1, 13=tumor allele2
+  # 5=chr, 6=start, 11=ref
   awk -F$'\t' '$11!=$'${allele_index?}'{print $5 "\t" $6 "\t\t" $11 "\t" $'${allele_index?}' "\t\t\tID=" "'${sample?}'" "@" NR "@" '${allele_index?}'}'
 }
 
 {
   echo -e "##fileformat=VCFv4.1\n#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO"
-  for f in $(ls ${input_dir?}/*.maf.gz); do
-    zcat $f | tail -n+3 | process $f 12
-    zcat $f | tail -n+3 | process $f 13
+  for f in $(ls ${input_dir?}/*.maf.gz | sort); do
+    zcat $f | tail -n+3 | process $f 12 # 12=Tumor_Seq_Allele1
+    zcat $f | tail -n+3 | process $f 13 # 13=Tumor_Seq_Allele2
   done | sort -t$'\t' -k1,1 -k2n,2n
 } | gzip -c
