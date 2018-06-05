de7bb78af8f3081c3eefde28ce15326333766fff
Anthony Cros <cros.anthony@gmail.com>
2018-06-05 10:12:13 -0400

    added unmapped record

--- liftover.sh
+++ liftover.sh
@@ -6,6 +6,6 @@ wget http://hgdownload.cse.ucsc.edu/goldenPath/hg19/liftOver/hg19ToHg38.over.cha
 
 echo -e "chr1\t103354135\t103354136\t1
 chr1\t104120227\t104120228\t2
-chr1\t104163266\t104163267\t3" > in.bed
+chr1\t120930088\t120930089\t52" > in.bed
 
 ./liftOver in.bed hg19ToHg38.over.chain.gz out.bed unmapped.bed # in.bed is chr,pos,pos+1,id
